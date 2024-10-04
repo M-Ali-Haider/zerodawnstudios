c60d9d86fe00561749b680d016a1b1fa16f467f6
M-Ali-Haider <mah01134212100@gmail.com>
2024-10-04 18:38:14 +0500

    les goooo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="h-screen text-6xl text-center flex items-center justify-center">
-      Those who waver are Weak - Monkey D. Garp
+    <main className="h-screen text-5xl sm:text-6xl text-center flex items-center justify-center">
+      Its Time B_tches.
     </main>
   );
 }

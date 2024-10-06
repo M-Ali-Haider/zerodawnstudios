8c31e77ec106ace2fdd772d370f0dfbcc2c20e49
M-Ali-Haider <mah01134212100@gmail.com>
2024-10-06 23:25:47 +0500

    ........

--- src/components/HorizontalScroll/card.jsx
+++ src/components/HorizontalScroll/card.jsx
@@ -1,7 +1,7 @@
 const HorizontalCard = ({ index, item }) => {
   return (
     <div
-      className={`min-w-full max-w-full h-full flex items-center justify-center font-bold text-5xl`}
+      className={`min-w-[calc(100%-16px)] md:min-w-[calc(50%-16px)] xl:min-w-[calc(33.33%-16px)] aspect-[621/400] flex items-center justify-center font-bold text-5xl rounded-3xl`}
       style={{ background: item.bg }}
     >
       HorizontalCard {index + 1}
--- src/components/HorizontalScroll/index.jsx
+++ src/components/HorizontalScroll/index.jsx
@@ -3,6 +3,7 @@ import { horizontalCards } from "@/utils/horizontalCards";
 import HorizontalCard from "./card";
 import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
 const HorizontalScroll = () => {
   const horizontalContainer = useRef(null);
@@ -11,12 +12,21 @@ const HorizontalScroll = () => {
     offset: ["start start", "end end"],
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["0vw", "-500vw"]);
+  const isXs = useMediaQuery({ query: "(min-width: 550px)" });
+  const isMd = useMediaQuery({ query: "(min-width: 768px)" });
+  const isLg = useMediaQuery({ query: "(min-width: 1024px)" });
+  const isXl = useMediaQuery({ query: "(min-width: 1280px)" });
+
+  const xValue = isXl ? "-100vw" : isLg ? "-200vw" : "-500vw";
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0vw", xValue]);
 
   return (
     <div className={`w-full h-[500vh] relative`} ref={horizontalContainer}>
-      <div className={`sticky top-0 w-full h-screen overflow-hidden`}>
-        <motion.div style={{ x }} className={`w-full h-full flex`}>
+      <div
+        className={`sticky top-0 w-full h-screen overflow-hidden flex items-center justify-center`}
+      >
+        <motion.div style={{ x }} className={`w-full flex ml-4 gap-4`}>
           {horizontalCards.map((item, index) => {
             return <HorizontalCard key={index} item={item} index={index} />;
           })}

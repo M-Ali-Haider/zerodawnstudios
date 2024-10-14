15d7c345cbd56ab2c197d0461121b592ff31b2ec
M-Ali-Haider <mah01134212100@gmail.com>
2024-10-14 14:27:58 +0500

    ........

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 // import Header from "@/components/Header";
 
-import { BottomSheet } from "@/components/BottomSheet";
+// import { BottomSheet } from "@/components/BottomSheet";
 
 // import Home from "@/components/Home";
+import Scene from "@/components/scene";
 export default function Homepage() {
   return (
     <>
       {/* <Header /> */}
-      <BottomSheet />
+      {/* <BottomSheet /> */}
       {/* <Home /> */}
+      <Scene />
     </>
   );
 }
---
+++ src/components/scene/index.jsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Scene() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/ZTnDYsGKhrpTmpWU/scene.splinecode" />
+    </main>
+  );
+}

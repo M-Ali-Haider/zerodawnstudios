d47c6da228a38f569a5786042bf10c3983e8b4b9
M-Ali-Haider <mah01134212100@gmail.com>
2024-11-06 22:20:24 +0500

    .........

--- src/components/scene/index.jsx
+++ src/components/scene/index.jsx
@@ -2,62 +2,57 @@
 import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMotionValue, useSpring, useTransform } from "framer-motion";
-import { Suspense, useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import Model from "./model";
 export default function Scene({ scrollYProgress }) {
-  const [dpr, setDpr] = useState(0.9);
   const rotationZ = useTransform(
     scrollYProgress,
     [0, 1],
     [Math.PI / 8, -Math.PI / 8]
   );
   const containerRef = useRef(null);
-  // const options = {
-  //   damping: 15,
-  // };
-  // const mouse = {
-  //   x: useSpring(useMotionValue(0), options),
-  //   y: useSpring(useMotionValue(0), options),
-  // };
+  const options = {
+    damping: 15,
+  };
+  const mouse = {
+    x: useSpring(useMotionValue(0), options),
+    y: useSpring(useMotionValue(0), options),
+  };
 
-  // const manageMouseMove = (e) => {
-  //   const { width, top, left, height } =
-  //     containerRef.current.getBoundingClientRect();
-  //   const { clientX, clientY } = e;
-  //   const x = -0.5 + (clientX - left) / width;
-  //   const y = -0.5 + (clientY - top) / height;
-  //   mouse.x.set(x);
-  //   mouse.y.set(y);
-  // };
+  const manageMouseMove = (e) => {
+    const { width, top, left, height } =
+      containerRef.current.getBoundingClientRect();
+    const { clientX, clientY } = e;
+    const x = -0.5 + (clientX - left) / width;
+    const y = -0.5 + (clientY - top) / height;
+    mouse.x.set(x);
+    mouse.y.set(y);
+  };
 
-  // const manageMouseLeave = () => {
-  //   mouse.x.set(0);
-  //   mouse.y.set(0);
-  // };
+  const manageMouseLeave = () => {
+    mouse.x.set(0);
+    mouse.y.set(0);
+  };
 
-  // useEffect(() => {
-  //   const container = containerRef.current;
-  //   container.addEventListener("mousemove", manageMouseMove);
-  //   container.addEventListener("mouseleave", manageMouseLeave);
-  //   return () => {
-  //     container.removeEventListener("mousemove", manageMouseMove);
-  //     container.removeEventListener("mouseleave", manageMouseLeave);
-  //   };
-  // }, []);
+  useEffect(() => {
+    const container = containerRef.current;
+    container.addEventListener("mousemove", manageMouseMove);
+    container.addEventListener("mouseleave", manageMouseLeave);
+    return () => {
+      container.removeEventListener("mousemove", manageMouseMove);
+      container.removeEventListener("mouseleave", manageMouseLeave);
+    };
+  }, []);
 
   return (
     <div
       ref={containerRef}
       className="h-screen min-w-[50vw] max-w-[50vw] relative"
     >
-      <Canvas dpr={dpr}>
+      <Canvas dpr={0.5}>
         <Environment preset="dawn" />
         <Suspense fallback={null}>
-          <Model
-            rotationZ={rotationZ}
-            // rotateX={mouse.y}
-            // rotateY={mouse.x}
-          />
+          <Model rotationZ={rotationZ} rotateX={mouse.y} rotateY={mouse.x} />
         </Suspense>
       </Canvas>
     </div>
--- src/components/scene/model.jsx
+++ src/components/scene/model.jsx
@@ -1,23 +1,19 @@
 import { useThree } from "@react-three/fiber";
-// import { motion } from "framer-motion-3d";
+import { motion } from "framer-motion-3d";
 import Meshes from "./meshes";
-const Model = ({
-  rotationZ,
-  // rotateX,
-  // rotateY
-}) => {
+const Model = ({ rotationZ, rotateX, rotateY }) => {
   const { viewport } = useThree();
   return (
-    <group
-      // rotation-y={rotateY}
-      // rotation-x={rotateX}
+    <motion.group
+      rotation-y={rotateY}
+      rotation-x={rotateX}
       scale={viewport.width / 8}
       dispose={null}
     >
       <group scale={0.01}>
         <Meshes rotationZ={rotationZ} />
       </group>
-    </group>
+    </motion.group>
   );
 };
 

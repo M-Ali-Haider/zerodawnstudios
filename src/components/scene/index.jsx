d5af38a2afd55a865f566e664f580d3c1b667bed
M-Ali-Haider <mah01134212100@gmail.com>
2024-11-06 22:15:42 +0500

    ........

--- src/components/scene/index.jsx
+++ src/components/scene/index.jsx
@@ -1,65 +1,64 @@
 "use client";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMotionValue, useSpring, useTransform } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import Model from "./model";
 export default function Scene({ scrollYProgress }) {
+  const [dpr, setDpr] = useState(0.9);
   const rotationZ = useTransform(
     scrollYProgress,
     [0, 1],
     [Math.PI / 8, -Math.PI / 8]
   );
   const containerRef = useRef(null);
-  const options = {
-    damping: 15,
-  };
-  const mouse = {
-    x: useSpring(useMotionValue(0), options),
-    y: useSpring(useMotionValue(0), options),
-  };
+  // const options = {
+  //   damping: 15,
+  // };
+  // const mouse = {
+  //   x: useSpring(useMotionValue(0), options),
+  //   y: useSpring(useMotionValue(0), options),
+  // };
 
-  const manageMouseMove = (e) => {
-    const { width, top, left, height } =
-      containerRef.current.getBoundingClientRect();
-    const { clientX, clientY } = e;
-    const x = -0.5 + (clientX - left) / width;
-    const y = -0.5 + (clientY - top) / height;
-    mouse.x.set(x);
-    mouse.y.set(y);
-  };
+  // const manageMouseMove = (e) => {
+  //   const { width, top, left, height } =
+  //     containerRef.current.getBoundingClientRect();
+  //   const { clientX, clientY } = e;
+  //   const x = -0.5 + (clientX - left) / width;
+  //   const y = -0.5 + (clientY - top) / height;
+  //   mouse.x.set(x);
+  //   mouse.y.set(y);
+  // };
 
-  const manageMouseLeave = () => {
-    mouse.x.set(0);
-    mouse.y.set(0);
-  };
+  // const manageMouseLeave = () => {
+  //   mouse.x.set(0);
+  //   mouse.y.set(0);
+  // };
 
-  useEffect(() => {
-    const container = containerRef.current;
-    container.addEventListener("mousemove", manageMouseMove);
-    container.addEventListener("mouseleave", manageMouseLeave);
-    return () => {
-      container.removeEventListener("mousemove", manageMouseMove);
-      container.removeEventListener("mouseleave", manageMouseLeave);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const container = containerRef.current;
+  //   container.addEventListener("mousemove", manageMouseMove);
+  //   container.addEventListener("mouseleave", manageMouseLeave);
+  //   return () => {
+  //     container.removeEventListener("mousemove", manageMouseMove);
+  //     container.removeEventListener("mouseleave", manageMouseLeave);
+  //   };
+  // }, []);
 
   return (
     <div
       ref={containerRef}
       className="h-screen min-w-[50vw] max-w-[50vw] relative"
     >
-      <Canvas
-        pixelRatio={Math.min(window.devicePixelRatio, 1.5)}
-        antialias={false}
-      >
-        <OrbitControls
-          enableRotate={false}
-          enableZoom={false}
-          enablePan={false}
-        />
+      <Canvas dpr={dpr}>
         <Environment preset="dawn" />
-        <Model rotationZ={rotationZ} rotateX={mouse.y} rotateY={mouse.x} />
+        <Suspense fallback={null}>
+          <Model
+            rotationZ={rotationZ}
+            // rotateX={mouse.y}
+            // rotateY={mouse.x}
+          />
+        </Suspense>
       </Canvas>
     </div>
   );
--- src/components/scene/model.jsx
+++ src/components/scene/model.jsx
@@ -1,40 +1,23 @@
 import { useThree } from "@react-three/fiber";
-import { motion } from "framer-motion-3d";
+// import { motion } from "framer-motion-3d";
 import Meshes from "./meshes";
-const Model = ({ rotationZ, rotateX, rotateY }) => {
+const Model = ({
+  rotationZ,
+  // rotateX,
+  // rotateY
+}) => {
   const { viewport } = useThree();
   return (
-    <motion.group
-      rotation-y={rotateY}
-      rotation-x={rotateX}
+    <group
+      // rotation-y={rotateY}
+      // rotation-x={rotateX}
       scale={viewport.width / 8}
       dispose={null}
     >
       <group scale={0.01}>
-        <spotLight
-          intensity={1.877}
-          angle={Math.PI / 6}
-          decay={2}
-          distance={7010}
-          color="#c483f2"
-          position={[940.784, 70.04, 389.408]}
-          rotation={[1.393, 0, -1.532]}
-        />
-
-        <spotLight
-          intensity={3}
-          angle={Math.PI / 6}
-          penumbra={0.2}
-          decay={2}
-          distance={8628}
-          color="#f3e650"
-          position={[751.69, -220.43, -298.17]}
-          rotation={[1.393, 0, -1.802]}
-        />
-
         <Meshes rotationZ={rotationZ} />
       </group>
-    </motion.group>
+    </group>
   );
 };
 

0b9ba1fd8c01b6b76fdab566b9969bc3d2973c18
M-Ali-Haider <mah01134212100@gmail.com>
2024-11-06 14:46:45 +0500

    .........

--- src/utils/maskCards.js
+++ src/utils/maskCards.js
@@ -1,28 +1,28 @@
 export const maskCards = [
   {
     name: "Elon Musk",
-    src: "/Mask/card1.png",
+    src: "/Mask/card1.webp",
     designation: "Name",
     desc: "I have never seen such a perfect studio. Zero Dawn captured my ideas exactly as I imagined",
     date: "Oct 24, 2023",
   },
   {
     name: "Elon Musk",
-    src: "/Mask/card2.png",
+    src: "/Mask/card2.webp",
     designation: "Name",
     desc: "I have never seen such a perfect studio. Zero Dawn captured my ideas exactly as I imagined",
     date: "Oct 24, 2023",
   },
   {
     name: "Elon Musk",
-    src: "/Mask/card3.png",
+    src: "/Mask/card3.webp",
     designation: "Name",
     desc: "I have never seen such a perfect studio. Zero Dawn captured my ideas exactly as I imagined",
     date: "Oct 24, 2023",
   },
   {
     name: "Elon Musk",
-    src: "/Mask/card4.png",
+    src: "/Mask/card4.webp",
     designation: "Name",
     desc: "I have never seen such a perfect studio. Zero Dawn captured my ideas exactly as I imagined",
     date: "Oct 24, 2023",

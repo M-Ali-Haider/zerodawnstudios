68d61f570bc4b5183cfa6face4d184c6a312c9ef
M-Ali-Haider <mah01134212100@gmail.com>
2025-01-22 21:19:27 +0500

    .......

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-transparent">
       <body className={`${SuisseIntl.className} ${SwearDisplay.variable}`}>
         <LenisScrollProvider>{children}</LenisScrollProvider>
       </body>

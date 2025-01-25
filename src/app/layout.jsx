515a14e38c1177cf91062ea15f348c83766267e0
M-Ali-Haider <mah01134212100@gmail.com>
2025-01-26 04:18:04 +0500

    .........

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,8 @@ import { SuisseIntl, SwearDisplay } from "./fonts/fonts";
 import "./globals.css";
 export const metadata = {
   title: "Zero Dawn Studios",
-  description: "Zero Dawn Studios Portfolio",
+  description:
+    "Zero Dawn Studios is a leading digital agency focused on branding, UI/UX design, mobile and web development.",
 };
 
 export default function RootLayout({ children }) {

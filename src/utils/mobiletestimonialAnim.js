1ced221d0bf8726baf43eff4e10e31c1fea506d1
M-Ali-Haider <mah01134212100@gmail.com>
2025-01-27 21:54:49 +0500

    ..........

--- src/utils/mobiletestimonialAnim.js
+++ src/utils/mobiletestimonialAnim.js
@@ -1,11 +1,11 @@
 export const mobTestimonialCardAnim = {
   initial: {
     opacity: 0,
-    y: "50px",
+    y: "60px",
   },
   open: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.5 },
+    transition: { duration: 0.75 },
   },
 };
